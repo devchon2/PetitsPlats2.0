fcaace7a71d85194f81ff8cceabfb912df5a5ddd
Rachid Chon <rachidchon.adm@hotmail.fr>
2023-08-19 15:24:34 +0200

    remise a niveau vers l'ancienne versions des ofnctions

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-syntax */
 
 function SearchFromMain(ValueToSearch, recipes) {
     const normalizedKeyword = Normalized(ValueToSearch);
@@ -12,146 +14,206 @@ function SearchFromMain(ValueToSearch, recipes) {
 
 
 
-function SearchFromIngredients(ValueToSearch, Actuals, recipes, Action = 'Add') {
-debugger;
-    const ActualsRecipes = Action === 'delete' ? recipes : Actuals;
 
-    const test = recipes.filter(recipe => 
-        ActualsRecipes.foreach(Recipe => 
-            Number(Recipe.id) === Number(recipe.id) && recipe.ingredients.some(ingr => Normalized(ingr.ingredient) === Normalized(ValueToSearch))))
-        return test;
-}
-
-
-function SearchFromUstensils(ValueToSearch, Actuals, recipes, Action = 'Add') {
-    const ActualsRecipes = Action === 'delete' ? recipes : Actuals;
-
-
-
-    const updatedArray = [];
-    const normalizedKeyword = Normalized(ValueToSearch);
-    recipes.forEach(recipe => {
-        const { id, ustensils } = recipe;
-        ActualsRecipes.forEach(Recipe => {
-
-            if (Number(Recipe.id) === Number(id)) {
-                ustensils.forEach(ustensil => {
-                    const normalizedElement = Normalized(ustensil);
-                    if (normalizedElement === normalizedKeyword && !updatedArray.includes(recipe)) {
-                        updatedArray.push(recipe);
-                    }
-                });
+function SearchFromIngredients(ValueToSearch, Actuals, recipes, from = 'add') {
+    let ActualsRecipe = Actuals;
+  
+    if (from === 'delete') {
+      ActualsRecipe = recipes;
+    }
+  
+    const UpdatedRecipes = [];
+    for (let j = 0; j < recipes.length; j += 1) {
+      const recipe = recipes[j];
+      const { id:id1, ingredients } = recipe;
+  
+      for (const ActualRecipe of ActualsRecipe) {
+        const {id:id2} = ActualRecipe;
+        
+        if (Number(id1) === Number(id2)) {
+  
+          for (let k = 0; k < ingredients.length; k += 1) {
+            const ingr = ingredients[k];
+            const { ingredient } = ingr;
+            const normalizedKeyword = Normalized(ValueToSearch);
+            const normalizedElement = Normalized(ingredient);
+  
+            if (normalizedElement === normalizedKeyword) {
+  
+              if (!UpdatedRecipes.includes(recipe)) {
+                UpdatedRecipes.push(recipe);
+  
+              }
             }
-        });
-    });
-    return updatedArray;
-}
-
-function SearchFromAppliances(ValueToSearch, Actuals, recipes, Action = 'Add') {
-    const ActualsRecipes = Action === 'delete' ? recipes : Actuals;
-
-
+          }
+        }
+      }
+    }
+  
+    return UpdatedRecipes;
+  }
+  
+  function SearchFromUstensils(ValueToSearch, Actuals, recipes, from = 'add') {
+    let ActualsRecipe = Actuals;
+  
+    if (from === 'delete') {
+      ActualsRecipe = recipes;
+    }
+  
     const updatedArray = [];
-    const normalizedKeyword = Normalized(ValueToSearch);
-    recipes.forEach(recipe => {
-        const { id, appliance } = recipe;
-
-        ActualsRecipes.forEach(Recipe => {
-
-            if (Number(Recipe.id) === Number(id)) {
-                const normalizedElement = Normalized(appliance);
-                if (normalizedElement === normalizedKeyword && !updatedArray.includes(recipe)) {
-                    updatedArray.push(recipe);
-                }
+    const normalizedKeyword = Normalized(ValueToSearch)
+  
+    for (const ActualRecipe of ActualsRecipe) {
+        const {id:id1} = ActualRecipe;
+  
+      for (const recipe of recipes) {
+        const { id:id2, ustensils } = recipe;
+  
+        if (Number(id1) === Number(id2)) {
+  
+          for (const ustensil of ustensils) {
+            const normalizedElement = Normalized(ustensil)
+            console.log(normalizedElement)
+            console.log(normalizedKeyword)
+            if (normalizedElement === normalizedKeyword) {
+  
+              if (!updatedArray.includes(recipe)) {
+                updatedArray.push(recipe)
+              }
             }
-        });
-    });
-    return updatedArray;
-}
-
-function SearchFromFilter(ValueToSearch, filterZone, recipes) {
+          }
+        }
+      }
+    }
+    return updatedArray
+  }
+  
+  function SearchFromAppliances(ValueToSearch, Actuals, recipes, from = 'add') {
+    let ActualsRecipe = Actuals;
+  
+    if (from === 'delete') {
+      ActualsRecipe = recipes;
+    }
+  
+    const updatedArray = [];
+    const normalizedKeyword = Normalized(ValueToSearch)
+  
+    for (const ActualRecipe of ActualsRecipe) {
+  
+      for (const recipe of recipes) {
+        const { id: id1, appliance } = recipe;
+        const {id:id2} = ActualRecipe;
+  
+        if (Number(id1) === Number(id2)) {
+          const normalizedElement = Normalized(appliance)
+  
+          if (normalizedElement === normalizedKeyword) {
+  
+            if (!updatedArray.includes(recipe)) {
+              updatedArray.push(recipe)
+  
+            }
+          }
+        }
+      }
+    }
+  
+    return updatedArray
+  }
+  
+  function SearchFromFilter(ValueToSearch, filterZone, recipes) {
     const Actuals = Array.from(document.querySelectorAll('.recipeCard'));
     let UpdatedRecipes;
+  
     if (filterZone === 'ingredients') {
-        UpdatedRecipes = SearchFromIngredients(ValueToSearch, Actuals, recipes);
+      UpdatedRecipes = SearchFromIngredients(ValueToSearch, Actuals, recipes);
+  
     } else if (filterZone === 'ustensils') {
-
-        UpdatedRecipes = SearchFromUstensils(ValueToSearch, Actuals, recipes);
+      UpdatedRecipes = SearchFromUstensils(ValueToSearch, Actuals, recipes);
+  
     } else if (filterZone === 'appliances') {
-
-        UpdatedRecipes = SearchFromAppliances(ValueToSearch, Actuals, recipes);
+      UpdatedRecipes = SearchFromAppliances(ValueToSearch, Actuals, recipes);
+  
     }
+  
     return UpdatedRecipes;
-}
-
-function SearchFromDeleteLabel(recipes) {
-    debugger;
-    const ActualsLabels = Array.from(document.querySelectorAll('.labels'));
-    let UpdatedFinalRecipes = [];
+  }
+  
+  function SearchFromDeleteLabel(recipes, MainInputValue) { // une fonction qui recuperes les labels et renvoi que les recettes qui contiennent l'ensemble des labels
+    const ActualsLabel = Array.from(document.querySelectorAll('.labels'));
+    debugger
+  
+    let UpdatedFinalRecipes = []
     let Action = 'delete';
+  
     let iteration = 0;
-
-    if (ActualsLabels.length < 1) {
-        UpdatedFinalRecipes = [...recipes];
-    
-    } else if (ActualsLabels.length > 0) {
+    for (const label of ActualsLabel) {
+      const name = label.getAttribute('data-normalized');
+      const type = label.getAttribute('data-type');
+      let updatedRecipes;
+      const ActualRecipes = Array.from(document.querySelectorAll('.recipeCard'));
+      if (iteration > 0) {
+        Action = 'add';
+      }
+  
+      if (type === 'ingredients') {
+        updatedRecipes = SearchFromIngredients(name, ActualRecipes, recipes, Action);
+  
+      } else if (type === 'ustensils') {
+        updatedRecipes = SearchFromUstensils(name, ActualRecipes, recipes, Action);
+  
+      } else {
+        updatedRecipes = SearchFromAppliances(name, ActualRecipes, recipes, Action);
+      }
+  
+      if (iteration === 0) { // si c'est la premiere iteration on met a jour le tableau final
+        UpdatedFinalRecipes.push(...updatedRecipes);
+        iteration += 1;
+  
+      } else{
         
-        ActualsLabels.forEach(label => {
-            const name = label.getAttribute('data-normalized');
-            const type = label.getAttribute('data-type');
-            const ActualRecipes = Array.from(document.querySelectorAll('.recipeCard'));
-            let updatedRecipes;
-
-            if (iteration > 0) {
-                Action = 'Add';
-            }
-
-            if (type === 'ingredients') {
-                updatedRecipes = SearchFromIngredients(name, ActualRecipes, recipes, Action);
-
-            } else if (type === 'ustensils') {
-                updatedRecipes = SearchFromUstensils(name, ActualRecipes, recipes, Action);
-
-            } else {
-                updatedRecipes = SearchFromAppliances(name, ActualRecipes, recipes, Action);
-            }
-
-            recipes.forEach(recipe => {
-
-                if (updatedRecipes.some(updatedRecipe => updatedRecipe.id === recipe.id && UpdatedFinalRecipes.includes(recipe))) {
-                    UpdatedFinalRecipes.push(recipe);
-                }
-            });            
-            iteration += 1;
-
-        })
+        for (const UpdatedFinalRecipe of UpdatedFinalRecipes) {
+        const { id: id1 } = UpdatedFinalRecipe
+        
+        for (const updrecipe of updatedRecipes) {
+          const { id: id2 } = updrecipe;
+          
+          if (id1 !== id2 && UpdatedFinalRecipes.includes(UpdatedFinalRecipe)) {
+            UpdatedFinalRecipes.pop(UpdatedFinalRecipe)
+          }
+        }
+      }
+    }}
+  
+    if (UpdatedFinalRecipes.length === 0) {
+      UpdatedFinalRecipes = SearchFromMain(MainInputValue, recipes);
     }
-
-
-
-    return UpdatedFinalRecipes;
-}
-
-
-function SearchListInput(filters, input) {
+  
+    return UpdatedFinalRecipes
+  }
+  
+  function SearchListInput(filters, input) {
+    // Fonction qui filtre les éléments de la liste des filtres
     if (input !== 0) {
+  
+      for (const filter of filters) {
+        const element = filter;
+        const normalizedElement = Normalized(element.textContent);
         const normalizedInput = Normalized(input);
-        filters.forEach(filter => {
-            const normalizedElement = Normalized(filter.textContent);
-            if (!normalizedElement.match(normalizedInput)) {
-                filter.classList.add('d-none');
-                filter.classList.remove('d-flex');
-            } else {
-                filter.classList.remove('d-none');
-                filter.classList.add('d-flex');
-            }
-        });
+        if (!normalizedElement.match(normalizedInput)) {
+          element.classList.add('d-none');
+          element.classList.remove('d-flex');
+        } else {
+          element.classList.remove('d-none');
+          element.classList.add('d-flex');
+        }
+      }
     }
-}
-
-// Fonction utilitaire pour normaliser les chaînes de caractères (enlever les accents et convertir en minuscules)
-function Normalized(str) {
+  }
+  
+  // Fonction utilitaire pour normaliser les chaînes de caractères (enlever les accents et convertir en minuscules)
+  function Normalized(str) {
     return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replaceAll(' ', '').replace('\'', '').toLowerCase().trim();
-}
-
-export { SearchFromFilter, SearchFromMain, SearchListInput, SearchFromDeleteLabel, Normalized };
+  }
+  
+  export { SearchFromFilter, SearchFromMain, SearchListInput, SearchFromDeleteLabel, Normalized };
